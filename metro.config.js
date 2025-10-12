b50958563edc1abb14f39d76272d7f0a230d7bf3
Your Name <anzellef0806@gmail.com>
2025-10-12 16:36:07 +0200

    fix(metro): broaden blockList to fix ENOSPC error

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,9 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// This is a workaround for the ENOSPC error on Termux
+config.resolver.blockList = [
+  /.*\/react-native\/ReactAndroid\/.*/,
+];
+
 module.exports = config;
