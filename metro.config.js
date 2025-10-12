08fb1f6aa152d2ac049f8389c5b29804f522fc10
Your Name <anzellef0806@gmail.com>
2025-10-12 20:17:06 +0200

    feat(viro): configure metro.config.js for AR assets

--- metro.config.js
+++ metro.config.js
@@ -3,6 +3,10 @@ const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
+// Add AR asset extensions
+config.resolver.assetExts.push('glb', 'vrx', 'bin');
+config.resolver.sourceExts.push('mjs');
+
 // This is a workaround for the ENOSPC error on Termux
 config.resolver.blockList = [
   /.*\/react-native\/ReactAndroid\/src\/main\/res\/systeminfo\/.*/,
