b6c979063ebc061eb502a5d493fd23eb49823c77
Your Name <anzellef0806@gmail.com>
2025-10-12 18:10:45 +0200

    fix(metro): refine blockList for ENOSPC and module resolution

--- metro.config.js
+++ metro.config.js
@@ -1,15 +1,13 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path'); // Import path module
 
 const config = getDefaultConfig(__dirname);
 
 // This is a workaround for the ENOSPC error on Termux
-config.resolver.blockList = []; // Clear blockList
-
-// Explicitly define watchFolders to reduce the number of watched files
-config.watchFolders = [
-  __dirname,
-  path.resolve(__dirname, 'node_modules'), // Watch node_modules
+config.resolver.blockList = [
+  /.*\/react-native\/ReactAndroid\/src\/main\/res\/systeminfo\/.*/,
+  /.*\/@firebase\/auth-compat\/node_modules\/@firebase\/auth\/dist\/cordova\/.*/,
+  /.*\/react-native\/ReactCommon\/react\/renderer\/components\/scrollview\/platform\/android\/react\/renderer\/components\/.*/,
+  /.*\/node_modules\/expo\/node_modules\/@expo\/cli\/build\/src\/start\/doctor\/apple\/.*/,
 ];
 
 module.exports = config;
