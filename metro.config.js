fbbef52a251494ec6feaf1b7c47e0f4fd0e775d1
Your Name <anzellef0806@gmail.com>
2025-10-12 17:34:09 +0200

    fix(metro): revert blockList to less aggressive state

--- metro.config.js
+++ metro.config.js
@@ -4,10 +4,10 @@ const config = getDefaultConfig(__dirname);
 
 // This is a workaround for the ENOSPC error on Termux
 config.resolver.blockList = [
-  /.*\/react-native\/ReactAndroid\/.*/,
+  /.*\/react-native\/ReactAndroid\/src\/main\/res\/.*/,
   /.*\/@firebase\/.*/,
-  /.*\/react-native\/ReactCommon\/.*/,
-  /.*\/node_modules\/expo\/node_modules\/@expo\/cli\/.*/,
+  /.*\/react-native\/ReactCommon\/react\/renderer\/components\/scrollview\/platform\/android\/react\/renderer\/components\/.*/,
+  /.*\/node_modules\/expo\/node_modules\/@expo\/cli\/build\/src\/start\/server\/metro\/debugging\/.*/,
 ];
 
 module.exports = config;
